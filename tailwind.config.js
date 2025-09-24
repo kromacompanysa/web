7162f05201f36fc4aeb1c21442939d5d517f314c
jorgeav527 <jorgeav527@gmail.com>
2025-09-24 16:02:11 -0500

    refactoruing static page

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./index.html", // HTML entry
+    "./src/**/*.{rs,html,js,ts}", // scan Rust, HTML, JS, TS
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
